ef806be622b6d9569d1160708ee7201f65547968
Jack <jhkent88@gmail.com>
2018-12-10 22:39:38 -0800

    Set up initial express server

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ROUTES
+// ==========================================
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+
+// server listen
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
